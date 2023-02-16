1c6ceaf89ab79a0a37e74597ffe61b0621821ad3
myraliym <myraliym@gmail.com>
2023-02-16 11:08:32 -0500

    commit for pic

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -9,18 +9,20 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(httr)
+library(xml2)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+NASA_APOD_20230216 <-
+  GET("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2023-02-16")
 
-
+content(NASA_APOD_20230216)$url|>
+  download.file(destfile = "inputs/NASA_APOD_20230216.jpg")
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+#write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
          
